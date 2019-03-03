8ff3d622b7e94b8e346822ac79911a7dd90a2891
Andrei0872 <andreigtj01@gmail.com>
2019-03-03 18:10:35 +0200

    Set up controllers dir

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,22 @@
-
 class Controller {
-    constructor () {}
+    constructor (name) {
+        const Service = require('../services');
+        this.service = new Service(name);
+    }
+
+    insertOne (req, res) {
+        const paramsBody = req.params.body;
+
+        this.service.insertOne(paramsBody);
+
+        res.json({
+            message: 'success'
+        })
+    }
+
+    default (req, res) {
+        res.send('hello from product controller!')
+    }
 }
 
-module.exports = new Controller();
+module.exports = Controller;
