e38a71014aad4ff5162a1ca497449d9c37c86c56
Andrei0872 <andreigtj01@gmail.com>
2019-03-07 00:24:03 +0200

    Add base btn component

---
+++ client/src/components/VButton.vue
@@ -0,0 +1,14 @@
+
+<script>
+export default {
+    name: 'base-button',
+    functional: true,
+    render (h, context) {
+        return h('button', 'btn')
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
