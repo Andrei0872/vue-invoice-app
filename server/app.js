c9f2db1d4ab97b4a245e4fbf29719422960083e8
Andrei0872 <andreigtj01@gmail.com>
2019-03-07 00:21:50 +0200

    Add cors to app.js

--- server/app.js
+++ server/app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const routes = require('./routes')
+const cors = require('cors')
 const debug = require('debug')('invoice-app');
 
 const app = express();
 debug('app initialized')
 
+const corsOptions = {
+    origin: 'http://localhost:8080'
+}
+
 // TODO: middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({  extended: false }))
 
 app.get('/', (req, res) => {
     res.send('ok!');
 });
 
+// const data = require('./mock');
 
 app.use('/', routes);
 
