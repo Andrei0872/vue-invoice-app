7409ad1d3c12104b00e6e25a06f7088a9dcd8406
Andrei0872 <andreigtj01@gmail.com>
2019-03-07 16:28:34 +0300

    Add styles to VButton

--- client/src/components/VButton.vue
+++ client/src/components/VButton.vue
@@ -3,12 +3,37 @@
 export default {
     name: 'base-button',
     functional: true,
-    render (h, context) {
-        return h('button', 'btn')
+    render (h, { listeners, children }) {
+        console.log(listeners)
+
+        const content = children[0].text;
+
+        return h('button', { on: { click: listeners.toggleState } }, content);
     }
 }
 </script>
 
 <style lang="scss" scoped>
-    
+    $button-color: darken($color: #394263, $amount: 5%);
+    $button-color-shadow: darken($color: #394263, $amount: 15%);
+
+    button {
+        padding: 8px;
+        display: inline-block;
+        background-color: $button-color;
+        color: #fff;
+        border-radius: 15px;
+        min-width: 1rem;
+        height: 40px;
+        outline: none;
+        border: none;
+        cursor: pointer;
+        letter-spacing: 1px;
+        font-size: 1rem;
+        box-shadow: 1px 2px 3px $button-color-shadow;
+
+        &:hover {
+            transform: scale(1.05);
+        }
+    }    
 </style>
