f76a1fa8520563a3c72f8539a9b4f686d387ccef
Andrei0872 <andreigtj01@gmail.com>
2019-03-10 22:02:48 +0200

    Add VTd component with `contenteditable` attr which acts like an input with v-model

---
+++ client/src/components/VTd.vue
@@ -0,0 +1,24 @@
+<template>
+    <td :contenteditable="contentEditable" @keyup="updateState($event)">
+        <slot></slot>
+    </td>
+</template>
+
+<script>
+export default {
+    props: {
+        contentEditable: {
+            type: Boolean,
+            default: false
+        },
+    },
+
+    methods: {
+        updateState (ev) {
+            const content = ev.target.textContent.trim();
+
+            this.$emit('update', content);
+        }
+    }
+}
+</script>
