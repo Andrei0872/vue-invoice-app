ba3a4075becadb8b896718c0ac371beb3adbbfdb
Andrei0872 <andreigtj01@gmail.com>
2019-03-10 20:59:34 +0200

    Switch to get for testing stuff

--- server/routes/Product.route.js
+++ server/routes/Product.route.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 const Controller = require('../controllers');
 const controller = new Controller('Product');
 
-router.post('/', controller.getAll.bind(controller))
+router.get('/', controller.getAll.bind(controller))
 router.get('/:id(\\d+)', controller.selectOneByID.bind(controller))
 
 module.exports = router;
