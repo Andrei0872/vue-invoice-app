e511ad7fae49f7a42aac6526539321b411421553
Andrei0872 <andreigtj01@gmail.com>
2019-03-10 19:28:09 +0200

    Avoid using unnecessary component: NewItem

--- client/src/views/Products.vue
+++ client/src/views/Products.vue
@@ -7,18 +7,26 @@
                 </VButton>
             </div>
             <div class="c-buttons__second">
-                <VButton :showBtn="isCreating" btnClass="success">
+                <VButton @createItem="createItem" :showBtn="isCreating" btnClass="success">
                     Create
                 </VButton>
             </div>
         </div>
+        <button @click="mustCreateItem = true">test create</button>
         <br><br>
+        
         <transition name="main" mode="out-in">
             <div v-if="!isCreating" key="table">
                 <VTable @showInfo="showInfo" :data="products" :fields="shownFields" />
             </div>
-            <NewItem v-else :fields="shownFields" :data="products" />
+            <div v-else key="create">
+                <div @click="addRow" class="icon">
+                    <font-awesome-icon icon="plus-circle" />
+                </div>
+                <VTable :mustCreateItem="mustCreateItem" :fields="shownFields" :data="newFields" />
+            </div>
         </transition>
+        
         <VModal :showModal="showDetails" @closeModal="closeModal">
             <template v-slot:header>
                 <div class="modal-header">
@@ -38,14 +46,13 @@
                     <div class="modal-body__arrow"><font-awesome-icon icon="arrow-right" /></div>
                     <div class="modal-body__value">
                         <span>{{ selectedProduct[field] }}</span>
-                        </div>
+                    </div>
                 </div>
             </template>
             <template v-slot:footer>
                 <!-- {{ selectedProduct }} -->
             </template>
         </VModal>
-        {{ selectedProduct }}
     </div>
 </template>
 
@@ -73,6 +80,8 @@ export default {
         isCreating: false,
         selectedProduct: {},
         showDetails: false,
+        newFields: [[]],
+        mustCreateItem: false,
     }),
 
     created () {
@@ -116,6 +125,17 @@ export default {
             this.showDetails = false;
             this.selectedProduct = {};
         },
+
+        addRow () {
+            this.newFields.push(
+                [{ id: this.newFields.length + 1 }]
+            )
+        },
+
+        createItem (ev) {
+            console.log(ev)
+            alert(1);
+        },
     },
 
     watch: {
@@ -139,11 +159,28 @@ export default {
 <style lang="scss" scoped>
     $modal-text-color: darken($color: #394263, $amount: 10%);
 
+     .icon {
+        padding: .9rem;
+
+        svg {
+            width: 32px;
+            height: 32px;
+            color: green;
+            border-radius: 50%;
+            background-color: darken($color: #fff, $amount: 8%);
+            cursor: pointer;
+
+            &:hover {
+                transform: scale(1.1);
+            }
+        }
+    }
+
     .c-buttons {
         display: flex;
     
         &__first {
-            flex-basis: 160px;
+            flex-basis: 180px;
         }
     }
 
