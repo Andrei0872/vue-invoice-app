d5572be1698948090aa31a671df61db6ffdb21f8
Andrei0872 <andreigtj01@gmail.com>
2019-03-11 23:49:30 +0200

    Select the row that is going to be updated and `blur` the others

--- client/src/components/VTable.vue
+++ client/src/components/VTable.vue
@@ -13,17 +13,22 @@
             </thead>
             <tbody :class="{ 'h-has-hover': !willCreate }">
                 <template v-for="(row, indexRow) in itemsCopy">
-                    <div v-if="willCreate && indexRow > 0" :key="row.id + 'icon'" class="icon">
-                        <font-awesome-icon @click="deleteRow(row.id)"  icon="minus-circle" />
+                    <div v-if="willCreate && indexRow > 0 || !willCreate" :key="row.id + 'icon'" class="icon">
+                        <font-awesome-icon 
+                            v-on="{ click: !willCreate ? updateRow.bind(null, row.id, row) : deleteRow.bind(null, row.id) }" 
+                            :class="isUpdating && selectedRowId === row.id ? 'times' : currentIcon"  
+                            :icon="isUpdating && selectedRowId === row.id ? 'times' : currentIcon" 
+                        />
                     </div>
                     <tr
+                    :class="selectedRowId === row.id ? 'selected' : isUpdating ?  'blurred' : null"
                     :key="row.id"
                 >
                     <VTd
-                        v-on="{ click: !willCreate ? $parent.showInfo.bind(null, row.id) : () => {} }"
+                        v-on="{ click: !willCreate && !isUpdating ? $parent.showInfo.bind(null, row.id) : () => {} }"
                         v-for="field in fields"
                         :key="field + row.id"
-                        :contentEditable="willCreate"
+                        :contentEditable="willCreate || isUpdating"
                         :isPlaceholder="typeof row[field] === 'undefined'"
                         @update="updateContent(indexRow, field, $event)"
                     >
@@ -39,10 +44,6 @@
 </template>
 
 <script>
-/* 
-<font-awesome-icon @click="deleteRow(row.id)" v-if="columnIndex === 0" icon="minus-circle" />
-*/
-
 import VTd from '../components/VTd';
 
 export default {
@@ -62,7 +63,16 @@ export default {
     data: () => ({
         itemsCopy: [],
         newItems: {},
+        selectedRowId: null,
+        selectedRow: [],
+        isUpdating: false,
     }),
+
+    computed: {
+        currentIcon () {
+            return this.willCreate ? 'minus-circle' : 'pencil-alt'
+        }
+    },
 
     watch: {
         // If the value is changed from true to false
@@ -83,6 +93,7 @@ export default {
 
     methods: {
         updateContent (rowIndex, field, val) {
+            this.isUpdating && (this.selectedRow[field] = val);
             // Get the field array and add the new item
             const arr = this.newItems[field];
             const updatedArr = this.$set(arr, rowIndex, val);
@@ -103,6 +114,40 @@ export default {
             this.itemsCopy = this.itemsCopy.filter(item => item.id !== id);
 
             this.$parent.newItems = [... this.itemsCopy];
+        },
+
+        compareUpdates (row) {
+            console.log(row)
+            console.log(this.selectedRow)
+        },
+
+        updateRow (id, row) {
+            let hasChanges = false;
+
+            // If a row is selected and the user clicks on another row
+            if (this.isUpdating && this.selectedRowId !== id) {
+                this.selectedRowId = id;
+                this.selectedRow = row;
+            } else {
+                this.isUpdating = !this.isUpdating
+
+                // this.selectedRowId = this.isUpdating ? id : null
+                this.isUpdating
+                    && (
+                        this.selectedRowId = id,
+                        this.selectedRow = row
+                    )
+                    || (
+                        this.selectedRowId = null,
+                        hasChanges = this.compareUpdates(row)
+                    ) 
+            }
+
+            /* 
+            if (hasChanges) {
+                TODO: api call: update data; but first, check if any changes have been made!
+            }
+            */
         }
     },
 
@@ -125,6 +170,15 @@ export default {
 
 <style lang="scss"scoped>
 
+.selected {
+    border: 1px solid blue;
+}
+
+.blurred {
+    filter: blur(2px);
+    background-color: lighten($color: #394263, $amount: 50%);
+}
+
 .placeholder {
     color: #ccc;
 }
@@ -137,12 +191,19 @@ export default {
     svg {
         width: 20px;
         height: 20px;
-        color: tomato;
-        background-color: #fff;
         border-radius: 50%;
 
         &:hover {
             transform: scale(1.1);
+        }
+
+        &.minus-circle {
+            color: tomato;
+            background-color: #fff;
+        }
+
+        &.pencil-alt {
+            color: rgb(51, 72, 99);
         }
     }
 }
