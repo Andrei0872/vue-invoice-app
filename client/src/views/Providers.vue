1e0a1924a81b5dc7d2127512afd775ae99fe695b
Andrei0872 <andreigtj01@gmail.com>
2019-03-13 01:22:53 +0200

    Add transition between creating and reading on Providers component

--- client/src/views/Providers.vue
+++ client/src/views/Providers.vue
@@ -1,15 +1,57 @@
 <template>
     <div>
-        Providers
+        <VContent :isCreating="isCreating" entityName="provider">
+            <template v-slot:existingItems>
+                <VTable @showInfo="showInfo" :items="items" :fields="fields" />
+            </template>
+            <template v-slot:createItems>
+                 <div @click="addRow" class="icon icon--add-row">
+                    <font-awesome-icon icon="plus-circle" />
+                </div>
+                <VTable :willCreate="isCreating" :items="newItems" :fields="fields" />
+            </template>
+        </VContent>
     </div>
 </template>
 
 <script>
+import VContent from '../components/VContent';
+import fetchMixin from '../mixins/fetchMixin';
+import VTable from '../components/VTable';
+
 export default {
     name: 'providers',
 
-    created () {
-        
-    }
+    components: { VContent, VTable },
+
+    mixins: [fetchMixin],
+
+    data: () => ({
+        items: [],
+        fields: [],
+        newItems: [{ id: 1 }],
+        entityName: 'provider',
+        isCreating: false
+    }),
+
+    methods: {
+        showInfo (id) {
+            // this.selectedProduct = this.products.find(product => product.id === id)
+            console.log(id)
+            // this.showDetails = true;
+        },
+
+        toggleState () {
+            this.isCreating = !this.isCreating
+        },
+
+        addRow () {
+
+        }
+    },
 }
 </script>
+
+<style lang="scss" scoped>
+    @import '../styles/common.scss';
+</style>
