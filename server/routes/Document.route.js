a1a0c02cab81d3eeccc2bce4db77980587aea066
Andrei0872 <andreigtj01@gmail.com>
2019-03-18 22:55:19 +0200

    Adapt to the new names of db tables

--- server/routes/Document.route.js
+++ server/routes/Document.route.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 const Controller = require('../controllers');
-const controller = new Controller('Document');
+const controller = new Controller('document');
 
 router.post('/', controller.getAll.bind(controller))
 // router.get('/:id(\\d+)', controller.selectOneByID.bind(controller))
--- server/routes/Provider.route.js
+++ server/routes/Provider.route.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 const Controller = require('../controllers');
-const controller = new Controller('Provider');
+const controller = new Controller('provider');
 
 router.post('/', controller.getAll.bind(controller))
 
