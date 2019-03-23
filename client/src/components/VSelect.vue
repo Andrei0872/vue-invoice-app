1460da6f64127f047fcb8f5e4de0c01f1b2c2abe
Andrei0872 <andreigtj01@gmail.com>
2019-03-23 19:30:39 +0200

    Add VSelect component

---
+++ client/src/components/VSelect.vue
@@ -0,0 +1,23 @@
+<template>
+    <select @change="$emit('addProvider', items[$event.target.selectedIndex])">
+        <option
+            v-for="item in items"
+            :key="item.id"
+            :value="item.id"
+        >
+            {{ item.name }}
+        </option>
+    </select>
+</template>
+
+<script>
+export default {
+    props: {
+        items: Array
+    },
+
+    created () {
+        this.$store.commit('SET_PROVIDER', this.items[0]);
+    }
+}
+</script>
