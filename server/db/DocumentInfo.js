45ffe9de9577378fe6bd7a4d2811b085e56bac8c
Andrei0872 <andreigtj01@gmail.com>
2019-03-27 00:23:32 +0200

    Modify Document DB stucture

--- server/db/DocumentInfo.js
+++ server/db/DocumentInfo.js
@@ -3,8 +3,6 @@ module.exports = {
         "id INT(8) AUTO_INCREMENT PRIMARY KEY",
         "provider_id INT(8) NOT NULL",
         "provider_name VARCHAR(50) NOT NULL",
-        "total_buy DECIMAL(7, 2)",
-        "total_sell DECIMAL(7, 2)",
         "invoice_number INT(10)",
         "inserted_date TIMESTAMP",
     ]
