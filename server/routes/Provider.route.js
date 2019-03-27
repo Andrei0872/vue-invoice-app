f6d96d2a5733ac9f29517843115bfa037789751a
Andrei0872 <andreigtj01@gmail.com>
2019-03-27 21:42:06 +0200

    Fetch from GET request

--- server/routes/Provider.route.js
+++ server/routes/Provider.route.js
@@ -3,7 +3,7 @@ const router = require('express').Router()
 const Controller = require('../controllers');
 const controller = new Controller('provider');
 
-router.post('/', controller.getAll.bind(controller))
+router.get('/', controller.getAll.bind(controller))
 router.post('/insert', controller.insertOne.bind(controller))
 router.put('/update', controller.updateOne.bind(controller))
 router.delete('/delete', controller.deleteOne.bind(controller))
