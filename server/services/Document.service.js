08a0ae1746b5181b84ed37dc9f784c21acb1b601
Andrei0872 <andreigtj01@gmail.com>
2019-03-27 16:32:21 +0200

    Update Document insertOne function according to the new db structure

--- server/services/Document.service.js
+++ server/services/Document.service.js
@@ -3,7 +3,7 @@ const mainService = require('./index');
 class DocumentService extends mainService {
     constructor (name) {
         super(name);
-        this.documentTableColumns = { provider_id: null, provider_name: null, total_buy: 0, total_sell: 0, invoice_number: null };
+        this.documentTableColumns = { provider_id: null, provider_name: null, invoice_number: null };
         this.documentProductTableColumns = ['document_id', 'product_id', 'quantity', 'quantity_type', 'buy_price', 'markup', 'sell_price','currency'];
     }
 
@@ -11,17 +11,14 @@ class DocumentService extends mainService {
         const documentValues = { ...this.documentTableColumns };
         
         const sanitizedItems = items.map(({ id, product_name: { id: product_id }, ...rest }) => {
-            documentValues['total_buy'] += +rest['buy_price'];
-            documentValues['total_sell'] += +rest['sell_price'];
-            
             return { product_id, ...rest }
         });
+
         documentValues['invoice_number'] = invoiceNr;
         documentValues['provider_name'] = provider_name;
         documentValues['provider_id'] = provider_id;
 
         try {
-            // select SUM(buy_price) as total_buy, SUM(sell_price) as total_sell from document_product group by document_id;
             const { insertId: lastInsertId } = await this.table.insertOne(
                 Object.keys(documentValues).join(', '),
                 [Object.values(documentValues)]
