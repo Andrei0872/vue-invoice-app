fa89f9bf7d1e4d0a476844a090020845ea762a1a
Andrei0872 <andreigtj01@gmail.com>
2019-03-27 21:39:54 +0200

    Add document mixin

---
+++ client/src/mixins/documentMixin.js
@@ -0,0 +1,20 @@
+export default {
+    data: () => ({
+        createColumns: [
+                "product_name",
+                "quantity",
+                "quantity_type",
+                "buy_price",
+                "markup",
+                "sell_price",
+                "currency"
+            ],
+            readColumns: [
+                "provider_name",
+                "total_buy",
+                "total_sell",
+                "invoice_number",
+                "inserted_date"
+            ]
+    })
+}
