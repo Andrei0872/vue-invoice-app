c1f995aea4862fff9dccb4ede1f575d540b57986
Andrei0872 <andreigtj01@gmail.com>
2019-03-28 02:54:19 +0200

    Add product names

--- client/src/store/modules/document_product.js
+++ client/src/store/modules/document_product.js
@@ -6,24 +6,28 @@ export const state = {
 }
 
 export const getters = {
-    getItemsById: state => state.items.filter(({ document_id }) => document_id === state.currentId)
+    getItemsById: state => state.items.filter(({ document_id }) => document_id === state.currentId),
 }
 
 export const mutations = {
     SET_ITEMS: (state, payload) => state.items.push(...payload),
     
-    SET_ID: (state, id) => state.currentId = id
+    SET_ID: (state, id) => state.currentId = id,
 }
 
 export const actions = {
-    fetchById: async ({ commit, dispatch, rootGetters }, id) => {
+    fetchById: async ({ commit, dispatch, rootGetters, rootState }, id) => {
         const url = `${rootGetters['api/mainURL']}/documents`;
         const config = { ...rootGetters['api/config'], body: JSON.stringify({ id }) };
 
         const data = await dispatch('api/makeRequest', { url, config }, { root: true });
 
-        commit('SET_ID', id);
-        commit('SET_ITEMS', data);
+        const products = rootState.product.items
+            .reduce((memo, product) => (memo[product.id] = product.name, memo), {})
+
+
+        commit('SET_ID', id);   
+        commit('SET_ITEMS', data.map(item => ({ ...item, product_name: products[item.product_id] })));
     },
 
     setId: ({ commit }, payload) => commit('SET_ID', payload)
