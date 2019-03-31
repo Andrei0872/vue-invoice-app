5bccad2b29a9f3b993d5f61b67bfaa646b55c500
Andrei0872 <andreigtj01@gmail.com>
2019-03-31 02:41:36 +0200

    Add overview Card component

---
+++ client/src/components/VCard.vue
@@ -0,0 +1,57 @@
+
+<script>
+export default {
+    functional: true,
+    
+    props: {
+        cardInfo: Object
+    },
+
+    render (h, { props: {cardInfo: { icon, title, value } } }) {
+        return (
+            <div class="card">
+                <div class="card__icon">
+                    <font-awesome-icon icon={icon} />
+                </div>
+                <div class="card__info">
+                    <div class="card__title"><strong>{title}</strong></div>
+                    <div class="card__value">{value}</div>
+                </div>
+            </div>
+        )
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .card {
+        background-color: #fff;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
+        transition: all .3s;
+
+        &:hover {
+            transform: translateY(-5px);
+            box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.16), 0 0 0 1px rgba(0, 0, 0, 0.08);
+        }
+
+        &__icon {
+            width: 60px;
+            height: 60px;
+            background-color: #394263;
+            border-radius: 50%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color: #fff;
+            font-size: 1.2rem;
+        }
+
+        &__card {
+            display: flex;
+            justify-content: column;
+        }
+    }
+</style>
