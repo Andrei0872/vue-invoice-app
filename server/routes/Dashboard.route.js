d5faadd399dd365467c725c7064685ddf358e4df
Andrei0872 <andreigtj01@gmail.com>
2019-04-02 22:28:16 +0300

    Change endpoint to `overview`

--- server/routes/Dashboard.route.js
+++ server/routes/Dashboard.route.js
@@ -3,6 +3,6 @@ const router = require('express').Router()
 const Controller = require('../controllers/Dashboard.controller');
 const controller = new Controller(null, 'Dashboard');
 
-router.get('/', controller.getMainOverview.bind(controller))
+router.get('/overview', controller.getMainOverview.bind(controller))
 
 module.exports = router;
