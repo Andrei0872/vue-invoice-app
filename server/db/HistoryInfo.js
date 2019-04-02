3f711bd6d70036d9c516e73d1bc8ec93d71d6c88
Andrei0872 <andreigtj01@gmail.com>
2019-04-03 01:03:18 +0300

    Add `action_type` column to `history` table

--- server/db/HistoryInfo.js
+++ server/db/HistoryInfo.js
@@ -2,7 +2,8 @@ module.exports = {
     fields: [
         'id int(8) auto_increment primary key',
         'message tinytext not null',
-        'enitity varchar(20) not null',
+        'entity varchar(20) not null',
+        'action_type varchar(20) not null',
         'inserted_date TIMESTAMP'
     ]
 }
