d145fcf07a5c435e3e7bedf19913456c830892f2
Andrei0872 <andreigtj01@gmail.com>
2019-04-04 12:36:21 +0300

    Add insert route to History entity

--- server/routes/History.route.js
+++ server/routes/History.route.js
@@ -4,5 +4,6 @@ const Controller = require('../controllers');
 const controller = new Controller('history');
 
 router.get('/', controller.getAll.bind(controller))
+router.post('/insert', controller.insertOne.bind(controller))
 
 module.exports = router;
