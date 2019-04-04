3e8a6139cdc40962cb232fb9d2be5beaa1596240
Andrei0872 <andreigtj01@gmail.com>
2019-04-04 20:46:20 +0300

    Make `fetchMainOverview` fn reusable

--- client/src/store/modules/dashboard.js
+++ client/src/store/modules/dashboard.js
@@ -4,7 +4,7 @@ export const state = {
     ['dashboard/overview']: {},
     vat: {},
     history: [],
-    needsUpdate: true
+    needsUpdate: false
 }
 
 export const getters = {
@@ -22,18 +22,23 @@ export const mutations = {
 }
 
 export const actions = {
-    fetchMainOverview: async ({ dispatch, commit, getters, rootGetters }) => {
+    fetchMainOverview: async ({ dispatch, commit, getters, rootGetters }, endpoint = null) => {
         const mainUrl = rootGetters['api/mainURL'];
         const reqConfig = { ...rootGetters['api/headers'], method: "GET"};
         const endpoints = getters.getEndpoints;
 
-        (await Promise.all(
-            endpoints.map(
-                endpoint => dispatch('api/makeRequest', { config: reqConfig, url: `${mainUrl}/${endpoint}` }, { root: true })
-            )
-        )).forEach(({ data }, index) => commit('SET_PROP_DATA', { stateProp: endpoints[index], payload: index !== 1 && data || data[0] }));
+        if (endpoint === null) {
+            (await Promise.all(
+                endpoints.map(
+                    endpoint => dispatch('api/makeRequest', { config: reqConfig, url: `${mainUrl}/${endpoint}` }, { root: true })
+                )
+            )).forEach(({ data }, index) => commit('SET_PROP_DATA', { stateProp: endpoints[index], payload: index !== 1 && data || data[0] }));
+        } else {
+            const { data } = await dispatch('api/makeRequest', { config: reqConfig, url: `${mainUrl}/${endpoint}` }, { root: true });
+            commit('SET_PROP_DATA', { stateProp: endpoint, payload: data });
+            commit('SET_UPDATE_STATE', !getters.getUpdateState)
+        }
 
-        commit('SET_UPDATE_STATE', !getters.getUpdateState)
     },
 
     setNewVat: ({ commit, state }, { type, value }) => {
