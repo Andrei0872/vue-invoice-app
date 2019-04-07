5d07853cfc9fbe7a7d05d9790bf54fc276243f25
Andrei0872 <andreigtj01@gmail.com>
2019-04-07 22:57:07 +0300

    View pdf/excel file in a separate window

--- client/src/views/File.vue
+++ client/src/views/File.vue
@@ -1,5 +1,43 @@
 <template>
-    <div>
-        {{ $route.params.fileType }} {{ $route.params.id }}
+    <div class="c-file">
+        <iframe
+            class="c-file__display"
+            v-if="src"
+            :src="src"
+        />
     </div>
 </template>
+
+<script>
+export default {
+
+    data: () => ({
+        src: null
+    }),
+
+    created () {
+        const url = `${this.$store.getters['api/mainURL']}/file/${this.$route.params.fileType}/${this.$route.params.id}`
+        const config = { headers: new Headers({
+            'Content-Type': 'application/pdf',
+        }), method: "POST" }
+
+        fetch(url, config)
+            .then(res => res.blob())
+            .then(res => {
+                const blob =  new Blob([res], {type: 'application/pdf'});
+                this.src = URL.createObjectURL(blob, { type: 'application/pdf' })
+            })
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .c-file {
+        height: 100%;
+
+        &__display {
+            width: 100%;
+            height: 100%;
+        }
+    }
+</style>
