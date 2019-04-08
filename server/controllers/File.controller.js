e7bf3edb29b99852bb2eab876fc7111235f826b3
Andrei0872 <andreigtj01@gmail.com>
2019-04-08 15:35:54 +0300

    Display data depending on file format(pdf / excel)

--- server/controllers/File.controller.js
+++ server/controllers/File.controller.js
@@ -1,20 +1,36 @@
-const service = require('../services/File.service');
-const DocumentService = require('../services/Document.service');
-const documentService = new DocumentService(null);
+const getDocumentData = require('../services/File.service');
+const fs = require('fs');
 
-const pdf = require('html-pdf')
 
 module.exports = async (req, res) => {
     const { fileType, id } = req.params;
+    const dataFetchedFn = await getDocumentData(id);
+    
+    
+    if (fileType === 'pdf') {
+        const pdf = require('html-pdf')
+        const content = dataFetchedFn('pdf');
+        
+        res.setHeader('Content-Type', 'application/pdf')
+        pdf.create(content).toStream( (err, stream) => {
+            stream.pipe(res);
+        });
+    } else {
+        const xlsx = require('xlsx')
 
-    const data = await documentService.getAllByDocument.call(documentService, id);
-    const fileName = `document${id}.pdf`;
+        const wb = xlsx.utils.book_new();
 
-    res.setHeader('Content-Type', 'application/json')
+        const table = dataFetchedFn('excel');
+        const ws = xlsx.utils.aoa_to_sheet(table);
+        xlsx.utils.book_append_sheet(wb, ws, 'document');
 
-    const content = `<b>andrei</b>`;
+        const wopts = { bookType: 'xlsx', bookSST: false, type: 'base64' };
+        const buffer = xlsx.write(wb, wopts);
 
-    pdf.create(content).toStream( (err, stream) => {
-        stream.pipe(res);
-    });
+        res.setHeader('Content-Disposition', ' attachment; filename=foo.xlsx');
+        res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+
+        res.end(Buffer.from(buffer, 'base64'));
+    }
+    
 }
--- server/services/File.service.js
+++ server/services/File.service.js
@@ -1,4 +1,21 @@
-module.exports = {
-    createPDF () {},
-    createExcel () {}
+const DocumentService = require('./Document.service');
+const documentService = new DocumentService(null);
+
+const getPDFContent = data => {
+    return '<b>andrei</b>'
+}
+const getExcelContent = data => {
+    return [Object.keys(data[0]), ...data.map(Object.values)]
+}
+
+const utils = {
+    'pdf': getPDFContent,
+    'excel': getExcelContent
+}
+
+module.exports = async id => {
+    const data = await documentService.getAllByDocument.call(documentService, id);
+    const dataWithoutId = data.map(({ id, ...row }) => row)
+
+    return type => utils[type](dataWithoutId)
 }
