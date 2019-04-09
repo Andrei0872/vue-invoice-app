46a32245e8845c4c1a8a51cb1d6f3ecee1a36541
Andrei0872 <andreigtj01@gmail.com>
2019-04-09 10:27:01 +0300

    Change route directions so the url will always append to the baseURL

--- client/src/sidebarLinks.js
+++ client/src/sidebarLinks.js
@@ -1,7 +1,7 @@
 export default [
     { id: 1, title: 'Dashboard', route: '/', icon: 'tachometer-alt' },
-    { id: 2, title: 'Products', route: 'products', icon: 'cart-plus' },
-    { id: 3, title: 'Providers', route: 'providers', icon: 'industry' },
-    { id: 4, title: 'Documents', route: 'documents', icon: 'file' },
-    { id: 5, title: 'Settings', route: 'settings', icon: 'cog' },
+    { id: 2, title: 'Products', route: '/products', icon: 'cart-plus' },
+    { id: 3, title: 'Providers', route: '/providers', icon: 'industry' },
+    { id: 4, title: 'Documents', route: '/documents', icon: 'file' },
+    { id: 5, title: 'Settings', route: '/settings', icon: 'cog' },
 ]
