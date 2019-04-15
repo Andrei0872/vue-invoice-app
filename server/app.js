7e1d17e2d2ab3b997a9b7a880231dfbc7f7c3c13
Andrei0872 <andreigtj01@gmail.com>
2019-04-16 01:09:27 +0300

    Let the server send the built files generated by client

--- server/app.js
+++ server/app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const routes = require('./routes')
 const cors = require('cors')
 const debug = require('debug')('invoice-app');
-
-// TODO: add morgan
+const path = require('path')
 
 const app = express();
 debug('app initialized')
@@ -15,11 +14,12 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({  extended: false }))
-
-// app.get('/', (req, res) => {
-//     res.send('ok!');
-// });
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/api', routes);
+
+// app.use('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 app.listen(3000, () => console.log('ok!!!'))
