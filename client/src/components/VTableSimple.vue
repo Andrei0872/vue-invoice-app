2d7e2618a04d788b751bac0e2159804ab066bdfa
Andrei0872 <andreigtj01@gmail.com>
2019-04-22 00:03:38 +0300

    Add VTableSimple component

---
+++ client/src/components/VTableSimple.vue
@@ -0,0 +1,18 @@
+<template functional>
+    <table class="table">
+        <thead>
+            <tr>
+                <th v-for="column in props.columns" :key="column">{{ column }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <slot name="tbody"></slot>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    props: { columns: Array }
+}
+</script>
