f8b7ca406f0f10103e9049e1f9889e116c33c0df
Andrei0872 <andreigtj01@gmail.com>
2019-07-05 15:20:45 +0300

    improvement(single-document): Define state's structure

--- client/src/store/modules/single-document/index.js
+++ client/src/store/modules/single-document/index.js
@@ -1,13 +1,24 @@
 export const namespaced = true;
 
 export const state = {
-    items: [],
-    deletedItems: [],
-    changes: {},
+    products: new Map(),
+    updatedProducts: new Map(),
+    createdProducts: new Map(),
+    deletedProducts: new Map(),
+
+    // ? needs a deeper look
+    // deletedItems: new Map(),
+    // changes: {},
     pristineData: new Map,
     currentId: null,
     alreadyFetched: false,
-    lastDeletedDocId: -1
+    lastDeletedDocId: -1,
+    // ? ===================
+
+    productsTracker: 1,
+    updatedProductsTracker: 1,
+    createdProductsTracker: 1,
+    deletedProductsTracker: 1,
 }
 
 export { getters } from './single-document.getters';
