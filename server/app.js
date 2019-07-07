f1a8a8e64a7434d3c4ada104b99e056267f3eed4
Andrei0872 <andreigtj01@gmail.com>
2019-07-07 16:58:48 +0300

    (server): Add morgan

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const routes = require('./routes')
 const cors = require('cors')
 const debug = require('debug')('invoice-app');
 const path = require('path')
+const morgan = require('morgan');
 
 const app = express();
 debug('app initialized')
@@ -11,6 +12,16 @@ const corsOptions = {
     origin: 'http://localhost:8080'
 }
 
+app.use(morgan((tokens, req, res) => {
+     return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        JSON.stringify(req.body) // Temporary
+    ].join(' ')
+}))
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({  extended: false }))
